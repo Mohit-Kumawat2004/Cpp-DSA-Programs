756dba358ca69441ab8ec8774897f4a45fd58623
Mohit-Kumawat2004 <159867125+Mohit-Kumawat2004@users.noreply.github.com>
2024-03-10 18:06:27 +0430

    Create Permutations-Strings.cpp
    Uses recursion + Backtracking

---
+++ Permutations-Strings.cpp
@@ -0,0 +1,39 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// Print all the permutations
+void getResult(string input, int l, int r)
+{
+   // Base case
+   if (l == r)
+   {
+       cout << input << ", ";
+       return;
+   }
+  
+   // All combinations
+   for (int i = l; i <= r; i++)
+   {
+       // Swap
+       swap(input[l], input[i]);
+
+       // Recursive call
+       getResult(input, l + 1 , r);
+
+       // Swap (backtracking)
+       swap(input[l], input[i]);
+   }
+   
+   return;
+}
+
+int main()
+{
+   string input = "code";
+   int n = input.length();
+ 
+   cout << "All Permutations:- " << endl;
+   getResult(input, 0, n - 1);
+   return 0;
+}
