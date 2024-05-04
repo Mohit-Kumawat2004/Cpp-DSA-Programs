9333f068987df697260a94eec6a35b4290be5321
Mohit Kumawat <159867125+Mohit-Kumawat2004@users.noreply.github.com>
2024-05-04 17:59:13 +0530

    Update Array To Variable conversion with Formula

---
+++ Array_To_Variable.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+#include <string>
+
+int main() {
+    int arr[] = {1, 2, 3, 4, 5};
+    int b ;
+    int n = sizeof(arr)/sizeof(arr[0]);
+    for(int i = 0; i < n ; i++) {
+        
+        b=b*10+arr[i]%10;   //math formula
+
+    }
+
+      cout <<"value of b is " << b << endl;
+    return 0;
+}
+
+
+
+/*
+
+Output : 
+
+value of b is 12345
+
+=== Code Execution Successful ===
+
+*/
+
+
+/*
+Another Method : 
+    int array[]={1,2,3,4,5};
+    string a;
+    int n = sizeof(arr)/sizeof(arr[0]);
+    for(int i = 0; i < n ; i++) {
+       a += to_string(arr[i]);
+    }
+    cout <<"value of a is "<<a<<endl;
+
+*/
+
+
+
